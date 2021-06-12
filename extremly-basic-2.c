4dc17ee37b8e436a7929fc83c305fa489da716aa
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-12 22:35:36 +0600

    solve 5 uri problem

---
+++ extremly-basic-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ int A;
+ scanf("%d",&A);
+ int B;
+ scanf("%d",&B);
+ int X = A + B;
+ printf("X = %d\n",X);
+
+    return 0;
+}
---
+++ simple-product-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ int a,b;
+ scanf("%d",&a);
+ scanf("%d",&b);
+ int PROD = a * b;
+ printf("PROD = %d\n",PROD);
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
