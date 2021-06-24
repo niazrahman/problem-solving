8a85719386566af2ef19caf92861225ad87ab7c5
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-24 19:58:45 +0600

    age in  days  probelm solved

---
+++ uri-online-judge/ageInDays.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n;
+    scanf("%d",&n);
+
+    int year = n/365;
+    int remain = n%365;
+    int month = remain/30;
+    int days = remain%30;
+
+    printf("%d ano(s)\n",year);
+    printf("%d mes(es)\n",month);
+    printf("%d dia(s)\n",days);
+ 
+    return 0;
+}
