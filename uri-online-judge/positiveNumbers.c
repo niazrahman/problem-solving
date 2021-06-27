4bc9fe8924491c7abfba1c33962c862c641ec7b5
niazrahman <b170403065@sw.jnu.ac.bd>
2021-06-27 20:46:09 +0600

    problem solved : positive numbers

---
+++ uri-online-judge/positiveNumbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n = 6, count=0;
+    double num;
+    for(int i =1;i<=n;i++){
+        scanf("%lf",&num);
+          if(num>0){
+            count++;    
+        }
+    }
+    printf("%d valores positivos\n",count);
+ 
+    return 0;
+}
