5fd2ad04cfe641d67f851cc504fefbbd095e73c8
niazrahman <b170403065@sw.jnu.ac.bd>
2021-07-14 21:07:06 +0600

    Below The Secundary Diagonal

---
+++ uri-online-judge/belowTheSecundaryDiagonal.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+int i,j;
+int elements=0;
+float num;
+float sum=0.0;
+char ch;
+scanf("%c",&ch);
+int spy=11;
+for(i=0;i<=11;i++){
+    for(j=0;j<=11;j++){
+        scanf("%f",&num);
+        if(i>0&&j>spy){
+            sum = sum + num;
+            elements++;
+        }
+    }
+    spy--;
+}
+if(ch=='S'){
+    printf("%.1f\n",sum);
+}else{
+    printf("%.1f\n",sum/elements);
+}
+
+    return 0;
+}
