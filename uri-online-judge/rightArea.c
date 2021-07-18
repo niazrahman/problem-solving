b2d6452d485eaf53c3500d88aea1da3a6bfed5e8
niazrahman <b170403065@sw.jnu.ac.bd>
2021-07-18 23:04:46 +0600

    right area

---
+++ uri-online-judge/rightArea.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+int i,j,elements;
+int spy = 11;
+float num,sum=0.0;
+char operation;
+scanf("%c",&operation);
+for(i=0;i<=11;i++){
+
+    for(j=0;j<=11;j++){
+            scanf("%f",&num);
+        if(i<j&&j>spy){
+           sum=sum+num;
+           elements++;
+        }  
+    }
+    spy--;
+ 
+}
+
+if(operation=='S'){
+    printf("%.1f\n",sum);
+}else{
+    printf("%.1f\n",sum/elements);
+}
+    return 0;
+}
