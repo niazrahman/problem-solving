2d13b849c25b9d85798f7da4271e2ff7d47e60b3
niazrahman <b170403065@sw.jnu.ac.bd>
2021-07-24 22:31:53 +0600

    Fix some issues

--- uri-online-judge/consumption.c
+++ uri-online-judge/consumption.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
- 
 int main() {
- 
-   int X;
-   scanf("%d",&X);
-   float Y;
-   scanf("%f",&Y);
-   float consumption = X/Y;
-   printf("%.3f km/l\n",consumption);
+    int X;
+    scanf("%d",&X);
+    float Y;
+    scanf("%f",&Y);
+    float consumption = X/Y;
+    printf("%.3f km/l\n",consumption);
     return 0;
 }
